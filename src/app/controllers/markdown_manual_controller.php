678511b2d9811486a7127926783a7fe4834c467e
Jaromir Tomek <jaromir.tomek@gmail.com>
2020-05-12 14:11:32 +0200

    Fixed PHP5.4 & PHP5.5 issue

--- src/app/controllers/markdown_manual_controller.php
+++ src/app/controllers/markdown_manual_controller.php
@@ -3,9 +3,9 @@ require_once(ATK14_DOCUMENT_ROOT."/app/controllers/md_book_base.php");
 
 class MarkdownManualController extends MdBookBaseController{
 
-	var $book_dir = ATK14_DOCUMENT_ROOT . "/public/markdown_manual/";
-
 	function _before_filter(){
+		$this->book_dir = ATK14_DOCUMENT_ROOT . "/public/markdown_manual/";
+
 		if(!$this->logged_user || !$this->logged_user->isAdmin()){
 			$this->_execute_action("error403");
 		}
