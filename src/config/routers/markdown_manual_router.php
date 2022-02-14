077c2ed7f7d14eb4069e403a1907a91943c1317f
Jaromir Tomek <jaromir.tomek@gmail.com>
2022-02-14 17:53:40 +0100

    Better routes

--- src/config/routers/markdown_manual_router.php
+++ src/config/routers/markdown_manual_router.php
@@ -4,10 +4,10 @@ class MarkdownManualRouter extends Atk14Router {
 	function setUp(){
 		// the nicest routes are for the default language
 		$this->addRoute("/markdown/","$this->default_lang/markdown_manual/index");
-		$this->addRoute("/markdown/<id>/","$this->default_lang/markdown_manual/detail");
+		$this->addRoute("/markdown/<id>/","$this->default_lang/markdown_manual/detail",array("id" => '/[^\/]{3,}/')); // we want to <id> not to be mistaken by <lang>
 
 		// fallback routes for other languages
-		$this->addRoute("/markdown-<lang>/","markdown_manual/index");
-		$this->addRoute("/markdown-<lang>/<id>/","markdown_manual/detail");
+		$this->addRoute("/markdown/<lang>/","markdown_manual/index");
+		$this->addRoute("/markdown/<lang>/<id>/","markdown_manual/detail",array("id" => '/[^\/]{3,}/'));
 	}
 }
